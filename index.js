651a6602c40180335a0fe39075c911b62428019a
Oskar Rough <oskar@rough.dk>
2022-12-18 14:27:20 +0100

    Update to ESM

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-require('dotenv').config()
-const got = require('got')
+import * as dotenv from 'dotenv'
+dotenv.config()
+import got from 'got'
 
 const {API_KEY, API_SECRET, CLOUD_NAME} = process.env
-const auth = `${API_KEY}:${API_SECRET}`
 const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/resources/image/upload?max_results=500`
 
-module.exports = async (req, res) => {
+if (!API_KEY) throw new Error('missing api key')
+if (!API_SECRET) throw new Error('missing api secret')
+if (!CLOUD_NAME) throw new Error('missing cloud name')
+
+async function main(req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*')
 	const response = await got(url, {
-		auth,
+		auth: `${API_KEY}:${API_SECRET}`,
 		json: true
 	})
 	return response.body
 }
 
+export default main
